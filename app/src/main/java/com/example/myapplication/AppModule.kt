6f31f6fb868c02cd6ad0950fe7087ff1071a230a
M-Jey <mjeyict3@gmail.com>
2025-01-25 21:37:52 +0800

    ETC: Added Comments

--- app/src/main/java/com/example/myapplication/AppModule.kt
+++ app/src/main/java/com/example/myapplication/AppModule.kt
@@ -13,6 +13,12 @@ import io.metamask.androidsdk.*
 @InstallIn(SingletonComponent::class)
 internal object AppModule {
 
+    /*
+        This function provides a DappMetadata object as a dependency for dependency injection.
+        It extracts the application name from the context and uses it to construct the DApp's name, URL,
+        and a static icon URL. The metadata is used to describe the DApp when interacting with wallets or
+        blockchain services.
+     */
      @Provides
          fun provideDappMetadata(@ApplicationContext context: Context): DappMetadata {
              return DappMetadata(
@@ -22,6 +28,12 @@ internal object AppModule {
              )
          }
 
+    /*
+        This function provides an instance of EthereumFlow, a wrapper around the Ethereum SDK.
+        It initializes the SDK with the application context, DApp metadata, and specific options
+        like the infuraAPIKey for connecting to the Ethereum network. It ensures that EthereumFlow
+        is properly configured for use in the app.
+     */
      @Provides
          fun provideEthereumFlow(@ApplicationContext context: Context, dappMetadata: DappMetadata): EthereumFlow {
              return EthereumFlow(
